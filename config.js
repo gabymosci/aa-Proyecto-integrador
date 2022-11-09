f3144ab7e30472baa8e4f204ab4d29c90ada70c1
gabymosci <gmosci@hotmail.com>
2022-11-09 08:26:20 -0300

    Antes productscart

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ sk_test_51M17KXDKMRiRpfMLZMVN7lC0fxYR2creNeaUVwYs1LYLT8U6sWIEpQDuwSy2TEX9ILkS3Va
 
 const config = {
     PORT: 8080,
-    PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MONGODB,    // 'MEM', 'FILE', 'MONGODB'
+    PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MEM,    // 'MEM', 'FILE', 'MONGODB'
     // MONGODB_CONNECTION_STR: 'mongodb://localhost/ecommerce',
     // MONGODB_CONNECTION_STR: 'mongodb+srv://juanromeroclases:HolaHola123@cluster0.5wxsavc.mongodb.net/ecommerce?retryWrites=true&w=majority',
-    MONGODB_CONNECTION_STR: 'mongodb+srv://gabymosci:gaby123@cluster0.r0ubpbm.mongodb.net/ecommerce?retryWrites=true&w=majority',
+    // MONGODB_CONNECTION_STR: 'mongodb+srv://gabymosci:gaby123@cluster0.r0ubpbm.mongodb.net/ecommerce?retryWrites=true&w=majority',
 
     MONGODB_TIMEOUT: 2000,  // Valor bajo para TESTING
 };
